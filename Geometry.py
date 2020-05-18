a8aeba3fd2208f6f1ff9af2b7f84910fe5f59e08
Florian Delrieu <florian.delrieu@free.fr>
2020-05-18 18:54:45 +0200

    [Minor Update] v1.1.1

--- Geometry.py
+++ Geometry.py
@@ -2,7 +2,7 @@
 Package de fonctions géometriques pour dessins 2D
 """
 
-def Circle(Center=tuple(),Radius=float()):
+def Circle(Center=tuple(),Radius=float(),LineStyle="-"):
     import numpy as np
     import matplotlib.pyplot as plt
     x_centre = Center[0]
@@ -10,7 +10,7 @@ def Circle(Center=tuple(),Radius=float()):
     theta = np.linspace(0,2*np.pi,100)
     x_cercle = x_centre + Radius*np.cos(theta)
     y_cercle = y_centre + Radius*np.sin(theta)
-    plt.plot(x_cercle,y_cercle,'-')
+    plt.plot(x_cercle,y_cercle,LineStyle)
 
 import matplotlib.pyplot as plt
 plt.figure()
