32827ded87fe800a4412a36b6ecbb87d360d18d2
Florian Delrieu <florian.delrieu@free.fr>
2020-05-19 19:30:27 +0200

    [ADD] v1.1.7 -- Improve Circle functions

--- Geometry.py
+++ Geometry.py
@@ -2,12 +2,21 @@
 Package de fonctions géometriques pour dessins 2D
 """
 
-def Circle(Center=tuple(),Radius=float(),LineStyle="-"):
+def Circle(Center=tuple(),Radius=float(),LineStyle="-",Nombre_points=100,LineWidth=0.5):
+    """
+    Creer un cercle avec un centre et un rayon
+    :param Center: tuple (x_centre,y_centre)
+    :param Radius: Rayon du cercle
+    :param LineStyle:
+    :param Nombre_points: Nombre de point pour tracer le cercle
+    :param LineWidth:
+    """
     import numpy as np
     import matplotlib.pyplot as plt
     x_centre = Center[0]
     y_centre = Center[1]
-    theta = np.linspace(0,2*np.pi,100)
+    theta = np.linspace(0,2*np.pi,Nombre_points)
     x_cercle = x_centre + Radius*np.cos(theta)
     y_cercle = y_centre + Radius*np.sin(theta)
-    plt.plot(x_cercle,y_cercle,LineStyle,LineWidth=0.5)
+    plt.plot(x_cercle,y_cercle,LineStyle,LineWidth=LineWidth)
+
