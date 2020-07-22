e9c58aa3886f346558eb1aaa7db7f7ac41d4bd38
floriandelrieu <florian.delrieu@free.fr>
2020-07-22 15:13:41 +0200

    [Update] v1.2.4 - comments

--- FFT.py
+++ FFT.py
@@ -4,9 +4,19 @@ Module pour analysse spectrales
 import numpy as np
 
 def fft(x):
+    """
+    A utiliser avec :freq: (Fast fourier transform)
+    :param x: array a analyser
+    :return: array contenant les coefficients de fourrier
+    """
     return 2*np.fft.fft(x)[0:len(x)//2+1]
 
 def freq(x,t=np.array([])):
+    """
+    :param x: array a analyser
+    :param t: vecteur temps
+    :return: array contenant la plage de fréquence
+    """
     if len(t) == 0: t = np.arange(len(x))
     assert len(t) == len(x) , "x and t vectors must be in same lenght"
     N = len(x)
@@ -14,5 +24,10 @@ def freq(x,t=np.array([])):
     return fs*np.arange(N/2+1)/N
 
 def psd(x):
+    """
+    A utiliser avec :freq:
+    :param x: array a analyser
+    :return: array contenant les psd
+    """
     fftx = fft(x)
     return abs(fftx/len(x))
