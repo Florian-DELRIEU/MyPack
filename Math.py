033c913948ac7582f057645de9320c4ef5576e4d
Florian Delrieu <florian.delrieu@free.fr>
2021-06-27 20:10:54 +0200

    add math functions
    - norm
    - dx
    - dxdy

--- Math.py
+++ Math.py
@@ -1,3 +1,19 @@
 """
 Diverses fonctions et méthode pour des analyses mathématiques
 """
+import numpy as np
+
+def norm(x,y,z=None):
+    if z == None:
+        return np.sqrt(x**2 + y**2)
+    else:
+        return np.sqrt(x**2 + y**2 + z**2)
+
+def dx(x):
+    x = np.array(x)
+    return x[1:] - x[:-1]
+
+def dxdy(x,y):
+    x = np.array(x)
+    y = np.array(y)
+    return (x[1:] - x[:-1]) / (y[1:] - y[:-1])
