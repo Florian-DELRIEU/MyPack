d8a852f709e3e590f1f96302ae01d6f2650345ba
Florian Delrieu <florian.delrieu@free.fr>
2021-06-28 23:42:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,10 +10,11 @@ DYDX = (y[1:] - y[:-1])/(x[1:] - x[:-1])
 a = (DYDX[-1] - DYDX[-2]) / (x[-1] - x[-2])
 b = DYDX[-1] - a * x[-1]
 last_dydx = a * x[-1] + b
-DYDX = np.array( list(DYDX) + [last_dydx])
+DYDX = np.array( list(DYDX).copy() + [last_dydx])
 
 plt.figure(1)
-plt.plot(x,y)
-plt.plot(x,DYDX)
+plt.clf()
+plt.plot(x,y,"+")
+plt.plot(x,DYDX,"+")
 
 plt.show()
