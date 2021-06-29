71160e4f81885897769b10d40dd37b4995b3eebd
Florian Delrieu <florian.delrieu@free.fr>
2021-06-29 22:41:56 +0200

    test Primitive 1A

--- test.py
+++ test.py
@@ -3,19 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x = np.linspace(0,10,1000)
+x = np.linspace(0,10,100)
 f = np.sin(x)
 deriv_f = np.cos(x)
+Int_f = - np.cos(x)
 
 dfdx = diff(f,x)
 
+####### FONCTION PRIMITIVE ################
+dx = np.array(diff(x))
+F = dx[:-1] * (f[1:] + f[:-1]) / 2
+###########################################
+
 plt.figure(1)
 plt.clf()
-plt.plot(x,f,"b-",label="f")
+plt.plot(x,f,"k-",label="f")
 plt.plot(x,deriv_f,"b--",label="df")
 plt.plot(x,dfdx,"b-x",label="dydx")
+plt.plot(x,Int_f,"r--",label="Int(F)")
+plt.plot(x[:-1],F,"r-x",label="F")
 
 plt.legend(loc="upper right")
 plt.show()
 
 print("Intrégale : ",Integr(f,x))
+
