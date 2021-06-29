52dda43812bc39923092100781d92edda052646c
Florian Delrieu <florian.delrieu@free.fr>
2021-06-29 22:18:33 +0200

    correct linear regression

--- Math.py
+++ Math.py
@@ -13,15 +13,15 @@ def diff(y,x=None):
     y = np.array(y)
     if x == None:
         DYDX = (y[1:] - y[:-1])
-        a = (DYDX[-1] - DYDX[-2]) # coeff directeur pour regression linéaire
+        DYDX = np.array( list(DYDX) + [DYDX[-1]] )
     else:
         x = np.array(x)
         DYDX = (y[1:] - y[:-1])/(x[1:] - x[:-1]) # len(DXDY) = len(x) - 1
     # Regression linéaire pour le dernier points pour avoir la meme longueur que x
         a = (DYDX[-1] - DYDX[-2])/(x[-2] - x[-3])
-    b = DYDX[-1] - a*x[-2]
-    last_dydx = a * x[-1] + b
-    DYDX = np.array( list(DYDX) + [last_dydx])
+        b = DYDX[-1] - a*x[-2]
+        last_dydx = a * x[-1] + b
+        DYDX = np.array( list(DYDX) + [last_dydx])
     return DYDX
 
 def Integral(y,x=None):
