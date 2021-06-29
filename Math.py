83b5dec3a6ba42d99c496a45197d42db59e5be48
Florian Delrieu <florian.delrieu@free.fr>
2021-06-29 22:15:59 +0200

    correct diff function
    Pour test de diff(y,x)

--- Math.py
+++ Math.py
@@ -13,11 +13,12 @@ def diff(y,x=None):
     y = np.array(y)
     if x == None:
         DYDX = (y[1:] - y[:-1])
+        a = (DYDX[-1] - DYDX[-2]) # coeff directeur pour regression linéaire
     else:
         x = np.array(x)
         DYDX = (y[1:] - y[:-1])/(x[1:] - x[:-1]) # len(DXDY) = len(x) - 1
     # Regression linéaire pour le dernier points pour avoir la meme longueur que x
-    a = (DYDX[-1] - DYDX[-2])/(x[-2] - x[-3])
+        a = (DYDX[-1] - DYDX[-2])/(x[-2] - x[-3])
     b = DYDX[-1] - a*x[-2]
     last_dydx = a * x[-1] + b
     DYDX = np.array( list(DYDX) + [last_dydx])
--- test.py
+++ test.py
@@ -7,17 +7,12 @@ x = np.linspace(0,10,10)
 y = x
 Y = -np.cos(x)
 
-dx = diff(x)
-
-Int = dx * (y[:-1] + y[1:]) / 2
+dydx = diff(x)
 
 plt.figure(1)
 plt.clf()
 plt.plot(x,y,"b-",label="f")
-plt.plot(x,Y,"b--",label="F")
-plt.plot(x[:-1],Int,"r-",label="I")
+plt.plot(x,dydx,"b--",label="df")
+
 plt.legend(loc="upper right")
-
 plt.show()
-
-print(Integral(y,x))
