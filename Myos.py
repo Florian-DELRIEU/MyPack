4c13d977297cffebb7b915a07a271b0ab444274c
floriandelrieu <florian.delrieu@free.fr>
2021-10-24 19:43:44 +0200

    minor fix

--- Myos.py
+++ Myos.py
@@ -5,20 +5,21 @@ def getOS():
     Verifie sur quelle machine le code est exécuté
     :return: :MAC: ou :PC:
     """
-    if os.getcwd()[0] == "":TERMINAL = "MAC"
+    if os.getcwd()[0] == "/":TERMINAL = "MAC"
     else:                   TERMINAL = "PC"
     return TERMINAL
 
 def goto_Onedrive():
-    if this_Terminal.os == "MAC":
+    TERMINAL = getOS()
+    if TERMINAL == "MAC":
         os.chdir("/Users/floriandelrieu/OneDrive/")
-    elif this_Terminal.os == "PC":
+    elif TERMINAL == "PC":
         os.chdir("D:/OneDrive/")
 
 def OneDrive_path():
-    if this_Terminal.os == "MAC":
+    if TERMINAL == "MAC":
         path = "/Users/floriandelrieu/OneDrive/"
-    elif this_Terminal.os == "PC":
+    elif TERMINAL == "PC":
         path = "D:/OneDrive/"
     return path
 
