b5f75e68114bbc3d52d1dbb354b76ae139183cab
Florian Delrieu <florian.delrieu@free.fr>
2023-04-22 14:12:24 +0200

    [v2.5.6] - add Testing file
    - test numpy array shape

---
+++ TestingHall/nump_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# a.shape is (5,4)
+A = []
+for _i in range(10):
+    i = str(_i)
+    A.append([
+        ["aa"+i,"ab"+i,"ac"+i],
+        ["ba"+i,"bb"+i,"bc"+i,],
+        ["ca"+i,"cb"+i,"cc"+i,],
+    ])
+
+A = np.array(A) # shape is (10,3,3)
+B = A.reshape(3,3,10)
+C = A.copy()
+
+# Affiche chaque éléments au cours du temps (ex aa(t), ab(t), etc...)
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        print(A[:,i,j]) #fixme (affiche comme je veux mais fais quand meme une erreur)
